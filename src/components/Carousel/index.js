7313b285f2524ddb6d95047e6f42ff759ab2bd8e
EMC <edwardcoad.ec@gmail.com>
2020-04-01 11:39:20 +1030

    clear carousel markers

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,7 +6,7 @@ import "./style.css";
 function Carousel() {
   return (
     <div id="indicators" className="carousel slide" data-ride="carousel">
-      <ol className="carousel-indicators">
+      <ol className="carousel-indicators indicatorLines">
           <li data-target="#indicators" data-slide-to="0" className="active"></li>
           <li data-target="#indicators" data-slide-to="1"></li>
           <li data-target="#indicators" data-slide-to="2"></li>
@@ -21,11 +21,11 @@ function Carousel() {
         })}
       </div>
       <a className="carousel-control-prev" href="#indicators" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="carousel-control-prev-icon indicatorLines" aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
       </a>
       <a className="carousel-control-next" href="#indicators" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="carousel-control-next-icon indicatorLines" aria-hidden="true"></span>
           <span className="sr-only">Next</span>
       </a>
   </div>
